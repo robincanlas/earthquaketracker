0e8b1b28c96bcd9fb66487ded1c9bc6cdec50dd4
Kristoffer Robin Canlas <kristofferrobincanlas@gmail.com>
2020-07-05 18:42:31 +0800

    added map and data

--- src/app/components/Map/index.tsx
+++ src/app/components/Map/index.tsx
@@ -1,92 +1,50 @@
 import * as React from 'react';
 import * as style from './style.css';
-import { Models } from 'app/models';
+// import { Models } from 'app/models';
 import mapboxgl, { LngLatLike } from 'mapbox-gl';
-// import { Feature, Geometry, GeoJsonProperties } from 'geojson';
-import useSWR from 'swr'; // React hook to fetch the data
-import lookup from 'country-code-lookup'; // npm module to get ISO Code for countries
 import axios from 'axios';
-import { useSelector, shallowEqual, useDispatch } from 'react-redux';
-import { RootState } from 'app/store';
 import { Icon } from 'semantic-ui-react';
-import { ActionTypes } from 'app/constants';
 
 // Mapbox css - needed to make tooltips work later in this article
 // import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicnVrYmluMDExIiwiYSI6ImNrYWdrbDI3bTA5NzgyeHBuaWkzbWIxeDQifQ.C7KY2elb_bs0qrST3HvSSQ';
 
-export const Map: React.FC = () => {
+export namespace Map {
+	export interface Props {
+		mapIsLoaded: () => void;
+	}
+}
+
+export const Map: React.FC<Map.Props> = (props: Map.Props) => {
 	// let map: mapboxgl.Map;
 	const mapboxElRef = React.useRef(null); // DOM element to render map
-	const [mapState, setMap] = React.useState<mapboxgl.Map | null>(null);
-	const fetcher = (url: string) =>
+	// const [mapState, setMap] = React.useState<mapboxgl.Map | null>(null);
+	const [data, setData] = React.useState<any>([]);
+
+	const fetchData = (url: string) => {
 		axios.get(url)
-			.then(r => r.data)
-			.then((country: Models.MapData[]) => {
-				localStorage.setItem('updated', new Date().toString());
-				const features = country.filter(element => {
-					return element.coordinates.latitude !== ''; // removed no coordinates
-				}).map((point, index) => (
-					{
-					'type': 'Feature' as const,
-					geometry: {
-						'type': 'Point' as const,
-						coordinates: [
-							point.coordinates.longitude,
-							point.coordinates.latitude
-						]
-					},
-					properties: {
-						'id': index, // unique identifier in this case the index
-						'country': point.country,
-						'province': point.province,
-						'cases': point.stats.confirmed,
-						'deaths': point.stats.deaths,
-						'recovered': point.stats.recovered,
-						'updated': new Date(point.updatedAt).toDateString()
-					}
-				}));
-				localStorage.setItem('data', JSON.stringify(features));
-				return features;
-			});
-		
-	const [shouldFetch, setShouldFetch] = React.useState<boolean>(false);
+		.then((res: any) => {
+			setData(res.data.features);
+		})
+		.catch((error: any) => {
+			throw new Error(error);
+		});
+	};
 
 	React.useEffect(() => {
-		// FETCH ONLY IF THE LAST UPDATED TIME IS GREATER THAN 4
-		if (localStorage.getItem('updated') && localStorage.getItem('data')) {
-			if (Math.floor(Math.abs(new Date().getTime() - new Date(localStorage.getItem('updated') as string).getTime()) / 3600000) > 4) {
-				setShouldFetch(true);
-			} else {
-				setShouldFetch(false);
-			}
-		} else {
-			setShouldFetch(true);
-		}
+		fetchData('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson');
 	}, []);
 	
-	// const { data } = useSWR('https://disease.sh/v2/jhucsse', fetcher);
-	const { data } = useSWR(shouldFetch ? 'https://disease.sh/v2/jhucsse' : null, fetcher);
-	
 	const constructMap = () => {
-		const covidData: any = data ? data : JSON.parse(localStorage.getItem('data') as string);
-		if (covidData) {
-			const highestCases: number = Math.max.apply(Math, covidData.map((o: any) => { return o.properties.cases; }));
-			const interpolateCount: number = 7;
-			const incremental: number = Math.floor(highestCases / interpolateCount);
-			let casesSample: number[] = [];
-			for (let i = 1; i <= interpolateCount; i++) {
-				casesSample.push(i * incremental);
-			}
-
+		if (data.length > 0) {
 			// You can store the map instance with useRef too
 			const map = new mapboxgl.Map({
 				container: mapboxElRef.current!,
-				style: 'mapbox://styles/rukbin011/ckagtrcc110de1ipt2pzqqn5v',
+				// style: 'mapbox://styles/rukbin011/ckagtrcc110de1ipt2pzqqn5v',
+				style: 'mapbox://styles/rukbin011/ckc8or1203fi81irsp4poh1hv',
 				center: [121.76572,  13.01153], // initial geo location for Philippines
-				zoom: 2 // initial zoom
-				// zoom: 4.94 // initial zoom
+				zoom: 4 // initial zoom
 			});
 
 			// Add navigation controls to the top right of the canvas
@@ -98,59 +56,81 @@ export const Map: React.FC = () => {
 				// with id 'points'
 				map.addSource('points', {
 					type: 'geojson',
+					generateId: true,
 					data: {
 						type: 'FeatureCollection',
 						// features: data as Feature<Geometry, GeoJsonProperties>[]
-						features: covidData as any // change later
+						features: data as any // change later
 					}
 				});
+				
+				// let radius = 1;
 
-				// Add our layer
 				map.addLayer({
-					id: 'circles',
-					source: 'points', // this should be the id of the source
-					type: 'circle',
-					// paint properties
-					paint: {
-						'circle-opacity': 0.75,
-						'circle-stroke-width': [
+					'id': 'earthquake-layer',
+					'type': 'circle',
+					'source': 'points',
+					'paint': {
+						'circle-opacity': 0.5,
+						'circle-stroke-width': 0.5,
+						'circle-color': [
 							'interpolate',
 							['linear'],
-							['get', 'cases'],
-							1, 1,
-							highestCases, 1.75,
+							['get', 'mag'],
+							// https://colorbrewer2.org/#type=sequential&scheme=YlOrRd&n=9 color palette
+							1, '#ffffcc',
+							2, '#ffeda0',
+							3, '#fed976',
+							4, '#feb24c',
+							5, '#fd8d3c',
+							6, '#fc4e2a',
+							7, '#e31a1c',
+							8, '#bd0026',
+							9, '#b10026',
+							10, '#b10026',
 						],
 						'circle-radius': [
 							'interpolate',
 							['linear'],
-							['get', 'cases'],
+							['get', 'mag'],
+							0, 2,
 							1, 4,
-							1000, 6,
-							4000, 8,
-							8000, 10,
-							12000, 15,
-							casesSample[0], 24,
-							casesSample[1], 28,
-							casesSample[2], 32,
-							casesSample[3], 36,
-							casesSample[4], 40,
-							casesSample[5], 44,
-							highestCases, 48
-						],
-						'circle-color': [
-							'interpolate',
-							['linear'],
-							['get', 'cases'],
-							1, '#ffffb2',
-							5000, '#fed976',
-							10000, '#feb24c',
-							25000, '#fd8d3c',
-							50000, '#fc4e2a',
-							75000, '#e31a1c',
-							highestCases, '#b10026'
-						],
+							2, 8,
+							3, 12,
+							4, 16,
+							5, 20,
+							6, 30,
+							7, 45,
+							8, 65,
+							9, 75,
+							10, 100
+						]
 					}
 				});
+
+				map.addLayer({
+					'id': 'outline',
+					'type': 'circle',
+					'source': 'points',
+					'paint': {
+						'circle-opacity': 0,
+						'circle-color': 'transparent',
+						'circle-stroke-width': 0.5,
+						'circle-radius': 0,
+						'circle-radius-transition': {
+							'duration': 0
+						}
+					}
+				});
+
+				// // Animate the circle
+				// setInterval(() => {
+				// 	map.setPaintProperty('outline', 'circle-radius', radius);
+				// 	radius = ++radius % 50;
+				// 	if (radius === 50) {
+				// 		map.setPaintProperty('outline', 'circle-opacity', 0);
+				// 	}
+				// }, 100);
 
 				// Create a mapbox popup
 				const popup = new mapboxgl.Popup({
@@ -158,17 +138,19 @@ export const Map: React.FC = () => {
 					closeOnClick: false
 				});
 
-				setMap(map as any);
+				// setMap(map as any);
+				props.mapIsLoaded();
 
 				// Variable to hold the active country/province on hover
-				let lastId: number | null;
+				let lastId: number | null = null;
 
 				// Mouse move event
-				map.on('mousemove', 'circles', e => {
+				map.on('mousemove', 'earthquake-layer', e => {
 					if (e.features && e.features[0].properties && e.features[0].geometry.type === 'Point') {
+						var features = map.queryRenderedFeatures(e.point);
+						console.log(features);
 						// Get the id from the properties
-						const id = e.features[0].properties.id;
-
+						const id = e.features[0].id as number;
 						// Only if the id are different we process the tooltip
 						if (id !== lastId) {
 							lastId = id;
@@ -176,28 +158,15 @@ export const Map: React.FC = () => {
 							// Change the pointer type on move move
 							map.getCanvas().style.cursor = 'pointer';
 
-							const { cases, deaths, country, province, recovered, updated }: any = e.features[0].properties;
+							const { mag, place, time }: any = e.features[0].properties;
 							const coordinates = e.features[0].geometry.coordinates!.slice();
-
 							// Get all data for the tooltip
-							const countryISO = lookup.byCountry(country)?.iso2 || lookup.byInternet(country)?.iso2;
 
-							const provinceHTML = province !== 'null' ? `<p>Province: <b>${province}</b></p>` : '';
+							const locationHtml = place !== 'null' ? `<p>Location: <b>${place}</b></p>` : '';
 
-							const mortalityRate = ((deaths / cases) * 100).toFixed(2);
-
-							const countryFlagHTML = Boolean(countryISO)
-								? `<img class=${style.flag} src="https://www.countryflags.io/${countryISO}/flat/64.png"></img>`
-								: '';
-
-								const HTML = `<p>Country: <b>${country}</b></p>
-												${provinceHTML}
-												<p class=${style.cases}>Cases: <b>${Number(cases).toLocaleString()}</b></p>
-												<p class=${style.deaths}>Deaths: <b>${Number(deaths).toLocaleString()}</b></p>
-												<p class=${style.recovered}>Recovered: <b>${Number(recovered).toLocaleString()}</b></p>
-												<p>Mortality Rate: <b>${mortalityRate}%</b></p>
-												<p>Updated: <b>${updated}</b></p>
-												${countryFlagHTML}`;
+								const HTML = `${locationHtml}
+												<p>Time: <b>${new Date(time).toDateString()} - ${new Date(time).toLocaleTimeString()}</b></p>
+												<p>Magnitude: <b>${mag}</b></p>`;
 
 							// Ensure that if the map is zoomed out such that multiple
 							// copies of the feature are visible, the popup appears
@@ -215,7 +184,7 @@ export const Map: React.FC = () => {
 				});
 
 				// Mouse leave event
-				map.on('mouseleave', 'circles', () => {
+				map.on('mouseleave', 'earthquake-layer', () => {
 					// Reset the last Id
 					lastId = null;
 					map.getCanvas().style.cursor = '';
@@ -225,52 +194,20 @@ export const Map: React.FC = () => {
 		}
 	};
 
-	// Initialize our map
+	// // Initialize our map
 	React.useEffect(() => {
-		constructMap();
+		if (data.length > 0) {
+			constructMap();
+		}
 	}, [data]);
 
-	const getCountry = (findCountry: string): string => {
-		switch (findCountry) {
-			case 'USA':
-				return 'US';
-			case 'UK':
-				return 'United Kingdom';
-			default: return findCountry;
-		}
-	};
-
-	const filteredCountry: string = useSelector((state: RootState) => state.statistic.country, shallowEqual);
-	// const countriesPayload: Models.CountriesPayload[] = useSelector((state: RootState) => state.countries.countriesPayload, shallowEqual);
-	React.useEffect(() => {
-		if (mapState && filteredCountry) {
-			let	filter: any = null;
-			if (filteredCountry !== 'Global') {
-				const newCountry: string = getCountry(filteredCountry);
-				filter = ['==', ['get', 'country'], newCountry];
-				const covidData: any = JSON.parse(localStorage.getItem('data') as string);
-				const country = covidData!.find((element: any) => element.properties.country === newCountry)!;
-				if (country) {
-					mapState.panTo([+country.geometry.coordinates[0], +country.geometry.coordinates[1]]);
-				}
-			}
-	
-			mapState.setFilter('circles', filter);
-		}
-	}, [filteredCountry, mapState]);
-
-	const dispatch = useDispatch();
-
 	const resetFilter = () => {
-		if (mapState) {
-			// mapState.setFilter('circles', null);
-			dispatch({ type: ActionTypes.SET_GLOBAL_STATISTICS });
-		}
+		// 
 	};
 
 	return (
 		<>
-			{filteredCountry !== 'Global' && <div onClick={resetFilter} className={style.repeat}>
+			{false && <div onClick={resetFilter} className={style.repeat}>
 				<Icon size='big' name='repeat' />
 			</div>}
 			<div id={style.map}>
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import * as style from './style.css';
-import { Map, Statistic, Countries, Chart } from 'app/components';
+import { Map } from 'app/components';
 import { Icon } from 'semantic-ui-react';
 
 export const App: React.FC = () => {
 	const [sidebar, setSidebar] = React.useState(false);
+	const [isLoading, setIsLoading] = React.useState(true);
 	const sidebarRef = React.useRef<HTMLDivElement | null>(null);
 
 	const handleMousedown = (e: MouseEvent) => {
@@ -21,19 +22,36 @@ export const App: React.FC = () => {
 		};
 	}, [sidebarRef, sidebar]);
 
+	const mapIsLoaded = () => {
+		setIsLoading(false);
+	};
+
 	return (
 		<React.Fragment>
+			{isLoading && <span className={style.loading}>
+				<div className={style['sk-cube-grid']}>
+					<div className={`${style['sk-cube']} ${style['sk-cube1']}`}></div>
+					<div className={`${style['sk-cube']} ${style['sk-cube2']}`}></div>
+					<div className={`${style['sk-cube']} ${style['sk-cube3']}`}></div>
+					<div className={`${style['sk-cube']} ${style['sk-cube4']}`}></div>
+					<div className={`${style['sk-cube']} ${style['sk-cube5']}`}></div>
+					<div className={`${style['sk-cube']} ${style['sk-cube6']}`}></div>
+					<div className={`${style['sk-cube']} ${style['sk-cube7']}`}></div>
+					<div className={`${style['sk-cube']} ${style['sk-cube8']}`}></div>
+					<div className={`${style['sk-cube']} ${style['sk-cube9']}`}></div>
+				</div>	
+			</span>}
 			<span className={`${style.settings} ${sidebar ? style.hide : ''}`} onClick={() => setSidebar(true)}>
 				<Icon name='settings' />
 			</span>
-			<Map />
+			<Map mapIsLoaded={mapIsLoaded} />
 			<div ref={sidebarRef} className={`${style.stats} ${sidebar ? style.show : ''}`}>
 				<span className={style.settings} onClick={() => setSidebar(false)}>
 					<Icon name='angle double up' />
 				</span>
-				<Statistic />
+				{/* <Statistic />
 				<Countries />
-				<Chart />
+				<Chart /> */}
 			</div>
 		</React.Fragment>
 	);
